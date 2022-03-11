709ca47749dcfb5c5f7ec18ad84f020ce0b9791c
Ankur Seth <iankurseth@gmail.com>
2022-03-11 12:57:19 +0530

    Moved PointsTableRow to separate file, moved addMatches() and setupNRR() logic to PointsTableRow

--- src/PointsTable/PointsTable.js
+++ src/PointsTable/PointsTable.js
@@ -1,42 +1,9 @@
+import PointsTableRow from './PointsTableRow';
+
 import OrangeCapTable from './OrangeCapTable';
 import PurpleCapTable from './PurpleCapTable';
 
 
-
-class PointsTableRow {
-	constructor (table, team) {
-		this.table = table;
-		this.team = team;
-		this.matches = [];
-		this.forInnings = [];
-		this.vsInnings = [];
-		this.batsFirst = 0;
-		this.bowlsFirst = 0;
-		this.points = 0;
-		this.wins = 0;
-		this.losses = 0;
-		this.noResults = 0;
-
-		this.forRuns = 0;
-		this.forBalls = 0;
-		this.forWickets = 0;
-		this.forRunrate = 0;
-
-		this.vsRuns = 0;
-		this.vsBalls = 0;
-		this.vsWickets = 0;
-		this.vsRunrate = 0;
-
-		this.netNunrate = 0;
-	}
-
-	getForRunRate = () => this.forRunrate.toFixed(2);
-	getVsRunRate = () => this.vsRunrate.toFixed(2);
-	getNetRunRate = () => this.netRunrate.toFixed(2);
-
-	getWinPercent = () => (this.wins * 100 / this.matches.length);
-	getWinPercentF = (n=1) => this.getWinPercent().toFixed(n);
-}
 
 export default class PointsTable {
 	constructor (tournament, matches) {
@@ -52,57 +19,6 @@ export default class PointsTable {
 
 		this.rows = [];
 		Object.keys(teamsObject).forEach(teamId => this.rows.push(new PointsTableRow(this, this.tournament.teams[teamId])));
-
-		this.matches.forEach(match => {
-			this.rows.forEach(row => {
-				if (match.team_a === row.team || match.team_b === row.team) {
-					row.matches += 1;
-
-					// adding points
-					if (match.winner === null) {
-						row.points += 1;
-						row.noResults += 1;
-					} else if (match.winner === row.team) {
-						row.points += 2;
-						row.wins += 1;
-					} else if (match.loser === row.team) {
-						row.losses += 1;
-					}
-
-					// adding runs, overs and wickets
-					let forInning = null, vsInning = null;
-					if (match.firstInning.team === row.team) {
-						row.batsFirst += 1;
-						forInning = match.firstInning;
-						vsInning = match.secondInning;
-					} else {
-						row.bowlsFirst += 1;
-						forInning = match.secondInning;
-						vsInning = match.firstInning;
-					}
-
-					if (forInning && forInning.actuallyHappened()) {
-						row.forInnings.push(forInning);
-						row.forRuns += forInning.runs;
-						row.forBalls += forInning.allout ? 120 : forInning.balls;
-						row.forWickets += forInning.wkts;
-					}
-
-					if (vsInning && vsInning.actuallyHappened()) {
-						row.vsInnings.push(vsInning);
-						row.vsRuns += vsInning.runs;
-						row.vsBalls += vsInning.allout ? 120 : vsInning.balls;
-						row.vsWickets += vsInning.wkts;
-					}
-				}
-			});
-		});
-
-		this.rows.forEach(row => {
-			row.forRunrate = row.forRuns * 6 / row.forBalls;
-			row.vsRunrate = row.vsRuns * 6 / row.vsBalls;
-			row.netRunrate = row.forRunrate - row.vsRunrate;
-		});
 
 		this.rows.sort((a, b) => {
 			if (a.points === b.points) return (b.netRunrate - a.netRunrate);
---
+++ src/PointsTable/PointsTableRow.js
@@ -0,0 +1,94 @@
+
+
+
+export default class PointsTableRow {
+	constructor (table, team) {
+		this.table = table;
+		this.team = team;
+		this.zeroInit();
+		this.addMatches();
+		this.setupNRR();
+	}
+
+	zeroInit () {
+		this.matches = [];
+		this.forInnings = [];
+		this.vsInnings = [];
+		this.batsFirst = 0;
+		this.bowlsFirst = 0;
+		this.points = 0;
+		this.wins = 0;
+		this.losses = 0;
+		this.noResults = 0;
+
+		this.forRuns = 0;
+		this.forBalls = 0;
+		this.forWickets = 0;
+		this.forRunrate = 0;
+
+		this.vsRuns = 0;
+		this.vsBalls = 0;
+		this.vsWickets = 0;
+		this.vsRunrate = 0;
+
+		this.netNunrate = 0;
+	}
+
+	addMatches () {
+		this.table.matches.forEach(match => {
+			if (match.team_a === this.team || match.team_b === this.team) {
+				this.matches.push(match);
+
+				// adding points
+				if (match.winner === null) {
+					this.points += 1;
+					this.noResults += 1;
+				} else if (match.winner === this.team) {
+					this.points += 2;
+					this.wins += 1;
+				} else if (match.loser === this.team) {
+					this.losses += 1;
+				}
+
+				// adding runs, overs and wickets
+				let forInning = null, vsInning = null;
+				if (match.firstInning.team === this.team) {
+					this.batsFirst += 1;
+					forInning = match.firstInning;
+					vsInning = match.secondInning;
+				} else {
+					this.bowlsFirst += 1;
+					forInning = match.secondInning;
+					vsInning = match.firstInning;
+				}
+
+				if (forInning && forInning.actuallyHappened()) {
+					this.forInnings.push(forInning);
+					this.forRuns += forInning.runs;
+					this.forBalls += forInning.allout ? 120 : forInning.balls;
+					this.forWickets += forInning.wkts;
+				}
+
+				if (vsInning && vsInning.actuallyHappened()) {
+					this.vsInnings.push(vsInning);
+					this.vsRuns += vsInning.runs;
+					this.vsBalls += vsInning.allout ? 120 : vsInning.balls;
+					this.vsWickets += vsInning.wkts;
+				}
+			}
+		});
+	}
+
+	setupNRR () {
+		this.forRunrate = this.forRuns * 6 / this.forBalls;
+		this.vsRunrate = this.vsRuns * 6 / this.vsBalls;
+		this.netRunrate = this.forRunrate - this.vsRunrate;
+	}
+
+	getForRunRate = () => this.forRunrate.toFixed(2);
+	getVsRunRate = () => this.vsRunrate.toFixed(2);
+	getNetRunRate = () => this.netRunrate.toFixed(2);
+
+	getWinPercent = () => (this.wins * 100 / this.matches.length);
+	getWinPercentF = (n=1) => this.getWinPercent().toFixed(n);
+}
