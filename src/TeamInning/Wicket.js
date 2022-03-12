44dfed31aec5570343354163e6c88f450e618146
Ankur Seth <iankurseth@gmail.com>
2022-03-12 23:41:03 +0530

    Implemted Wicket.getRRF() for use in WicketsTable component

--- src/TeamInning/Wicket.js
+++ src/TeamInning/Wicket.js
@@ -3,11 +3,17 @@ export default class Wicket {
 	constructor (teamInning, jo) {
 		this.teamInning = teamInning;
 		this.batsman = teamInning.tournament.players[jo.id];
-		this.over = jo.over;
+
+		// this.over goes from 0-19, instead of 1-20
+		this.over = jo.over - 1;
 		this.ball = jo.ball;
 		this.runs = jo.r;
 		this.wicket = jo.w;
 	}
+
+	getBalls = () => (this.over * 6 + this.ball);
+	getRR = () => (this.runs * 6 / this.getBalls());
+	getRRF = (n=1) => this.getRR().toFixed(n);
 
 	setBatsmanInning (batsmanInning) {
 		// let outType = batsmanInning.jo.out.type;
