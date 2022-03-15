7b807dde3ea2af6e8e54d5a9f099cb133aa61f7b
Ankur Seth <iankurseth@gmail.com>
2022-03-15 15:41:11 +0530

    Added top10BowlingPerformances to PurpleCapTable

--- src/PointsTable/PurpleCapTable.js
+++ src/PointsTable/PurpleCapTable.js
@@ -5,6 +5,19 @@ import BaseCapTable from './BaseCapTable';
 export default class PurpleCapTable extends BaseCapTable {
 	constructor (pointsTable) {
 		super(pointsTable);
+
+		this.bowlingPerformances = [];
+		for (let match of this.pointsTable.matches) {
+			this.bowlingPerformances = this.bowlingPerformances.concat(match.bowlingPerformances);
+		}
+
+		this.sortedBowlingPerformances = [...this.bowlingPerformances].sort((a, b) => {
+			if (b.wickets === a.wickets) {
+				return a.runs - b.runs;
+			}
+			return b.wickets - a.wickets;
+		});
+		this.top10BowlingPerformances = this.sortedBowlingPerformances.slice(0, 10);
 	}
 }
 
