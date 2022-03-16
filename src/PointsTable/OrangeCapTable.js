2fdfbdf718e27e1a7761d3cb9d1dae77917b107d
Ankur Seth <iankurseth@gmail.com>
2022-03-16 17:19:19 +0530

    Added battingRecords to OrangeCapTable

--- src/PointsTable/OrangeCapTable.js
+++ src/PointsTable/OrangeCapTable.js
@@ -1,4 +1,5 @@
 import BaseCapTable from './BaseCapTable';
+import BattingRecord from '../Player/BattingRecord';
 
 
 
@@ -15,8 +16,18 @@ export default class OrangeCapTable extends BaseCapTable {
 		this.performances = this.performances.filter(b => !b.dnb);
 		this.sortedPerformances = [...this.performances].sort((a, b) => (b.runs - a.runs));
 		this.top10Scores = this.sortedPerformances.slice(0, 10);
+
+		this.records = [];
+		const batsmanIds = {};
+		this.performances.forEach(x => batsmanIds[x.player.id] = true);
+		for (const batsmanId in batsmanIds) {
+			const player = this.pointsTable.tournament.players[batsmanId];
+			this.records.push(new BattingRecord(player, this.performances, x => x.player === player));
+		}
+		this.sortedRecords = [...this.records].sort((a, b) => (b.runs - a.runs));
 	}
 
 	getTopNScores = (n=10) => this.sortedPerformances.slice(0, n);
+	getTopNByRuns = (n=10) => this.sortedRecords.slice(0, n);
 }
 
